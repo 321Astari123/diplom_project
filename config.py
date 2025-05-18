61fafd0a42e9f038340aa55dad4426dffcd91ba3
321Astari123 <68735994+321Astari123@users.noreply.github.com>
2025-05-18 17:57:57 +0300

    fixconfig

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class Config:
     
     # База данных
     MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '')
+    MYSQL_USER = os.getenv('MYSQL_USER', 'online_library_user')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '12345678')
     MYSQL_DB = os.getenv('MYSQL_DB', 'online_library')
     
     # Загрузка файлов
